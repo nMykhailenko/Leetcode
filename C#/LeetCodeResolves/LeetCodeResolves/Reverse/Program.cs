5f7b8a1b05ebe598318e001c0de89ac2dec6c03b
Nikita Mykhailenko <nickitamykhailneko@gmail.com>
2020-12-04 17:19:51 +0100

    add solution for ReverseInt32 task

---
+++ C#/LeetCodeResolves/LeetCodeResolves/Reverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCodeResolves.Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Reverse(1534236469);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        static int Reverse(int x)
+        {
+            try
+            {
+                var chars = Math.Abs(x).ToString().ToCharArray();
+                Array.Reverse(chars);
+                var resultAsStr = new string(chars);
+                var result = x > 0 ? Convert.ToInt32(resultAsStr) : Convert.ToInt32($"-{resultAsStr}");
+                return result;
+            }
+            catch(OverflowException ex)
+            {
+                return 0;
+            }
+        }
+    }
+}
