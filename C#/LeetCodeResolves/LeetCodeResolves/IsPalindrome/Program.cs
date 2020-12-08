898713093b3f35d647542dc564ff51bf1ab234fd
Nikita Mykhailenko <nickitamykhailneko@gmail.com>
2020-12-08 14:32:41 +0100

    add solution for IsPalindrome task

---
+++ C#/LeetCodeResolves/LeetCodeResolves/IsPalindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeResolves.IsPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 121;
+            var result = IsPalindrome(n);
+            Console.ReadKey();
+        }
+
+        static bool IsPalindrome(int x)
+        {
+            var n = x;
+            var rev = 0;
+            while (x > 0)
+            {
+                var dig = x % 10;
+                rev = rev * 10 + dig;
+                x /= 10;
+            }
+
+            return rev == n;
+        }
+    }
+}
