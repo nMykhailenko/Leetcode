85f48d90fc48d1512fa0867dcf1347b7f8f74138
Nikita Mykhailenko <nickitamykhailneko@gmail.com>
2020-12-09 08:56:15 +0100

    solution for MaxArea task

---
+++ C#/LeetCodeResolves/LeetCodeResolves/MaxArea/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeResolves.MaxArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = new int[] { 1, 8, 3, 4, 8, 5, 7 };
+            var result = MaxArea(n);
+            Console.ReadKey();
+        }
+        
+        static int MaxArea(int[] height)
+        {
+            int maxarea = 0, l = 0, r = height.Length - 1;
+            while (l < r)
+            {
+                maxarea = Math.Max(maxarea, Math.Min(height[l], height[r]) * (r - l));
+                if (height[l] < height[r])
+                    l++;
+                else
+                    r--;
+            }
+
+            return maxarea;
+        }
+    }
+}
