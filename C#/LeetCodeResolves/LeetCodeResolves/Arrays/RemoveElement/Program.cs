9940d8254dff374c332b027313a384e5c6a09ad3
Nikita Mykhailenko <nickitamykhailneko@gmail.com>
2021-02-11 21:51:27 +0100

    remove element from array task

---
+++ C#/LeetCodeResolves/LeetCodeResolves/Arrays/RemoveElement/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeResolves.Arrays.RemoveElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 0 , 1 , 0, 1 , 2, 5, 6 };
+            var result = RemoveElement(nums, 1);
+
+            Console.ReadLine();
+        }
+
+        static int RemoveElement(int[] nums, int val)
+        {
+            var i = 0;
+            var n = nums.Length;
+            while (i < n)
+            {
+                if (nums[i] == val)
+                {
+                    nums[i] = nums[n - 1];
+                    n--;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return n;
+        }
+    }
+}
