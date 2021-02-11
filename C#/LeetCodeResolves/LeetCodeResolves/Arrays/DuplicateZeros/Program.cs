f694b107d6fd823cc53668d957ebf2723bd59319
Nikita Mykhailenko <nickitamykhailneko@gmail.com>
2021-02-11 21:21:09 +0100

    add solution for array lessong - insert operation

---
+++ C#/LeetCodeResolves/LeetCodeResolves/Arrays/DuplicateZeros/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeResolves.Arrays.DuplicateZeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { -5, -3, 0, 3, 5 };
+            DuplicateZeros(nums);
+
+            Console.ReadLine();
+        }
+
+        static void DuplicateZeros(int[] arr)
+        {
+            var possibleDups = 0;
+            var length = arr.Length - 1;
+
+            for (var left = 0; left <= length - possibleDups; left++)
+            {
+                if (arr[left] == 0)
+                {
+                    if (left == length - possibleDups)
+                    {
+                        arr[length] = 0;
+                        length -= 1;
+                        break;
+                    }
+                    possibleDups++;
+                }
+            }
+
+            var last = length - possibleDups;
+            for (var i = last; i >= 0; i--)
+            {
+                if (arr[i] == 0)
+                {
+                    arr[i + possibleDups] = 0;
+                    possibleDups--;
+                    arr[i + possibleDups] = 0;
+                }
+                else
+                {
+                    arr[i + possibleDups] = arr[i];
+                }
+            }
+        }
+    }
+}
---
+++ C#/LeetCodeResolves/LeetCodeResolves/Arrays/Merge/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeResolves.Arrays.Merge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums1 = new int[] { 1, 6, 10, 0, 0 };
+            var nums2 = new int[] { 2, 11 };
+            var m = 3;
+            var n = 2;
+            Merge(nums1, m, nums2, n);
+
+            Console.ReadLine();
+        }
+
+        static void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            var len = n + m;
+            var i = m - 1;
+            var j = n - 1;
+            var k = 1;
+
+            while(i >=0 || j >= 0)
+            {
+                if(i >= 0 && j >= 0)
+                {
+                    if(nums1[i] < nums2[j])
+                    {
+                        nums1[len - k] = nums2[j--];
+                    }
+                    else
+                    {
+                        nums1[len - k] = nums1[i--];
+                    }
+
+                    k++;
+                    continue;
+                }
+
+                if (i >= 0)
+                {
+                    nums1[len - k] = nums1[i--];
+                    k++;
+                }
+
+                if (j >= 0)
+                {
+                    nums1[len - k] = nums2[j--];
+                    k++;
+                }
+            }
+        }
+    }
+}
