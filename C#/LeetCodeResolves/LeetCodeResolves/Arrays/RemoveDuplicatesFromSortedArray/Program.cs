d1978089986e437c8b4917c3c0e48d759c9ddcbf
Nikita Mykhailenko <nickitamykhailneko@gmail.com>
2021-02-14 21:56:37 +0100

    remove duplicates from sorted array

---
+++ C#/LeetCodeResolves/LeetCodeResolves/Arrays/RemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeResolves.Arrays.RemoveDuplicatesFromSortedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            var result = RemoveDuplicates(nums);
+
+            Console.ReadLine();
+        }
+
+        static int RemoveDuplicates(int[] nums)
+        {
+            var n = nums.Length;
+            if (n == 0) return 0;
+
+            var i = 0;
+            for(var j = 1; j < n; j++)
+            {
+                if(nums[j] != nums[i])
+                {
+                    i++;
+                    nums[i] = nums[j];
+                }
+            }
+
+            return i + 1;
+        }
+    }
+}
