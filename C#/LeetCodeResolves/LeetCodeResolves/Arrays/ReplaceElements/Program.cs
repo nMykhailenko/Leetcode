a83cccea0c413048c0fec3c89aa4824f52802878
Nikita Mykhailenko <nickitamykhailneko@gmail.com>
2021-02-16 00:18:19 +0100

    add solution for replace elements and valid mountain tasks

---
+++ C#/LeetCodeResolves/LeetCodeResolves/Arrays/ReplaceElements/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeResolves.Arrays.ReplaceElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 0, 3, 2, 1 };
+            var result = ReplaceElements(nums);
+
+            Console.ReadLine();
+        }
+
+        static int[] ReplaceElements(int[] arr)
+        {
+            if (arr.Length == 1) return new int[] { -1 };
+            for(var i = 0; i < arr.Length; i++)
+            {
+                if (i == arr.Length - 1) 
+                { 
+                    arr[i] = -1; 
+                }
+                else
+                {
+                    var maxValue = FindMax(arr, i + 1);
+                    arr[i] = maxValue;
+                }
+            }
+
+            return arr;
+        }
+
+        static int FindMax(int[] arr, int startPosition)
+        {
+            var maxValue = 0;
+            for(var i = startPosition; i < arr.Length; i++)
+            {
+                maxValue = Math.Max(arr[i], maxValue);
+            }
+
+            return maxValue;
+        }
+    }
+}
---
+++ C#/LeetCodeResolves/LeetCodeResolves/Arrays/ValidMountainArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeResolves.Arrays.ValidMountainArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 0, 3, 2, 1 };
+            var result = ValidMountainArray(nums);
+
+            Console.ReadLine();
+        }
+
+        static bool ValidMountainArray(int[] arr)
+        {
+            var n = arr.Length;
+            var i = 0;
+
+            while (i + 1 < n && arr[i] < arr[i + 1])
+                i++;
+
+            if (i == 0 || i == n - 1)
+                return false;
+
+            while (i + 1 < n && arr[i] > arr[i + 1])
+                i++;
+
+            return i == n - 1;
+        }
+    }
+}
