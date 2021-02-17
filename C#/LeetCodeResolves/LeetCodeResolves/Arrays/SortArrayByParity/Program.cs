9e81108eacce4d05e674b257a8f5dc0b2be589a4
Nikita Mykhailenko <nickitamykhailneko@gmail.com>
2021-02-17 23:41:12 +0100

    add solution for SortArrayByParity

---
+++ C#/LeetCodeResolves/LeetCodeResolves/Arrays/SortArrayByParity/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeResolves.Arrays.SortByParity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 3, 1, 2, 4 };
+            var result = SortArrayByParity(arr);
+
+            Console.ReadLine();
+        }
+
+        static int[] SortArrayByParity(int[] arr)
+        {
+            var slow = 0;
+            var fast = 0;
+            while (fast < arr.Length)
+            {
+                if (arr[slow] % 2 != 0)
+                {
+                    while (fast < arr.Length && arr[fast] % 2 != 0)
+                    {
+                        fast++;
+                    }
+
+                    if (fast == arr.Length)
+                    {
+                        return arr;
+                    }
+                    else
+                    {
+                        var temp = arr[slow];
+                        arr[slow] = arr[fast];
+                        arr[fast] = temp;
+                    }
+                }
+
+                slow++;
+                fast++;
+            }
+
+            return arr;
+        }
+    }
+}
