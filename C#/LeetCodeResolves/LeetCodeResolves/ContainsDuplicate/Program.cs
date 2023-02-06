611482b34ddb8326cf4f4d302b63bd8b675da67c
mykhailenkoplat4me <mykyta.m@plat4me.com>
2023-02-06 16:38:49 +0100

    add solution for contains duplicate.

---
+++ C#/LeetCodeResolves/LeetCodeResolves/ContainsDuplicate/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCodeResolves.ContainsDuplicate
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] {2, 5, 1, 1, 4, 7};
+            var result = ContainsDuplicate(nums);
+            Console.ReadLine();
+        }
+        
+        static bool ContainsDuplicate(int[] nums) {
+            var dictionary = new Dictionary<int, int>();
+
+            foreach(var num in nums){
+                if(dictionary.ContainsKey(num)){
+                    return true;
+                }
+        
+                dictionary.Add(num, 0);
+            }
+
+            return false;
+        }    }
+}
