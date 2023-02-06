0dccda3a12b6d4517e29288b84297ee531893e61
mykhailenkoplat4me <mykyta.m@plat4me.com>
2023-02-06 16:36:53 +0100

    add shuffle array solution.

---
+++ C#/LeetCodeResolves/LeetCodeResolves/ShuffleTheArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCodeResolves.ShuffleTheArray
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] {2, 5, 1, 3, 4, 7};
+            var result = Shuffle(nums, 3);
+            Console.ReadLine();
+        }
+        
+        static int[] Shuffle(int[] nums, int n) {
+            var half = (long)(nums.Length / 2);
+            var result = new int[nums.Length];
+           
+            var current = 0;
+            var pair = half;
+            for(var i = 0; i < half; i++){
+                result[current] = nums[i];
+                result[current + 1] = nums[pair];
+    
+                current += 2;
+                pair += 1;
+            }
+
+            return result;
+        }
+    }
+}
