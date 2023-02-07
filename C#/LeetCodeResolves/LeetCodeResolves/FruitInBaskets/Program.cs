ecb3bc8ec0843221c222f507d353eae0c920f5e3
mykhailenkoplat4me <mykyta.m@plat4me.com>
2023-02-07 14:08:24 +0100

    add solution for Fruit in Basket.

---
+++ C#/LeetCodeResolves/LeetCodeResolves/FruitInBaskets/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeResolves.FruitInBaskets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fruits = new int[] {3,3,3,1,2,1,1,2,3,3,4};
+            var result = TotalFruit(fruits);
+
+            Console.ReadLine();
+        }
+
+        static int TotalFruit(int[] fruits)
+        {
+            int left = 0, right = 0, cnt = 0, res = 0;
+            var dic = new Dictionary<int,int>();
+            while(right < fruits.Length)
+            {
+                if(dic.ContainsKey(fruits[right]))
+                    dic[fruits[right]]++;
+                else
+                    dic.Add(fruits[right], 1);             
+            
+                if(dic[fruits[right]]== 1)
+                    cnt++;
+            
+                right++;
+            
+                while(cnt > 2)
+                {
+                    dic[fruits[left]]--;
+                    if(dic[fruits[left]] == 0)
+                        cnt--;
+                    left++;
+                }
+            
+                res = Math.Max(res, right - left);
+            }
+            
+            return res;
+        }
+    }
+}
