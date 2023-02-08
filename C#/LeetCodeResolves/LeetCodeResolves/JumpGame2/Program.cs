94393d3dbcf7ea56d7dfb83f871f4756d0aab56d
mykhailenkoplat4me <mykyta.m@plat4me.com>
2023-02-08 22:48:13 +0100

    add solution for Jump Game ||

---
+++ C#/LeetCodeResolves/LeetCodeResolves/JumpGame2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeResolves.JumpGame2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 2,3,1,1,4};
+            var result = Jump(nums);
+            Console.WriteLine($"Expected: 2, Actual: {result}");
+            Console.ReadLine();
+        }
+
+        static int Jump(int[] nums)
+        {
+            var steps = 0;
+
+            var currEnd = 0;
+            var currFar = 0;
+
+            for(var i = 0; i < nums.Length - 1; i++) {
+                currFar = Math.Max(currFar, i + nums[i]);
+
+                if(i == currEnd){
+                    steps++;
+                    currEnd = currFar;
+                }
+            }
+
+            return steps;
+        }
+    }
+}
